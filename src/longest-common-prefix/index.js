31e9e4e11018b0e07cbfa667dc32d2f049325db5
Yevhen <elaichenkov@gmail.com>
2020-07-17 22:45:05 +0300

    update(prettier): not use tab

--- src/longest-common-prefix/index.js
+++ src/longest-common-prefix/index.js
@@ -1,19 +1,19 @@
 // https://leetcode.com/problems/longest-common-prefix/
 
 function getLongestCommonPrefix(strings) {
-  if (!strings.length) return '';
+	if (!strings.length) return '';
 
-  let commonPrefix = strings[0];
+	let commonPrefix = strings[0];
 
-  for (let i = 0; i < commonPrefix.length; i++) {
-    for (const string of strings) {
-      if (commonPrefix[i] !== string[i]) {
-        return commonPrefix.slice(0, i);
-      }
-    }
-  }
+	for (let i = 0; i < commonPrefix.length; i++) {
+		for (const string of strings) {
+			if (commonPrefix[i] !== string[i]) {
+				return commonPrefix.slice(0, i);
+			}
+		}
+	}
 
-  return commonPrefix;
+	return commonPrefix;
 }
 
 module.exports = getLongestCommonPrefix;
